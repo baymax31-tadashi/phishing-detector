3dac2227f199681d4735082ee843333212c5f71b
baymax31-tadashi <briddhima31@gmail.com>
2026-01-23 13:34:43 +0530

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -21,13 +21,13 @@ def extract_features_from_url(url):
     ]
 
 def extract_text_from_image(image_path):
-    # Load image using OpenCV
+   
     image = cv2.imread(image_path)
-    # Convert to grayscale
+    
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # Optional preprocessing
+    
     gray = cv2.GaussianBlur(gray, (3, 3), 0)
-    # OCR
+    
     text = pytesseract.image_to_string(gray)
     return text
 
